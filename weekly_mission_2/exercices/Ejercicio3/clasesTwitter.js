5e70e7757ebd13e02ff0b35f4f8250b0d0c6d566
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-20 16:04:22 -0500

    trendingTopic convertido a clase

--- weekly_mission_2/exercices/Ejercicio3/clasesTwitter.js
+++ weekly_mission_2/exercices/Ejercicio3/clasesTwitter.js
@@ -13,7 +13,18 @@ class User {
     }
 }
 
-
+class Trending_Topic {
+    constructor(topic, tweets, location, position){
+        this.topic = topic
+        this.tweets = tweets
+        this.location = location
+        this.position = position
+    }
+    getGeneralInfo(){
+        return `The trending topic: ${this.topic} has ${this.tweets} tweets on ${this.location}`
+    }
+}
 
 const user = new User("Daniel", "dalexez", "Mexico", 71, 69, 50, 32)
-console.log(user)
+const trendingTopic = new Trending_Topic("Reforma Energetica", 12000, "Mexico", 1)
+console.log(trendingTopic)
