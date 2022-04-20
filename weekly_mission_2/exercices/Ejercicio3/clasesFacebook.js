588812f8ec838d80c1eb1ef035f549eaa59c2fca
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-20 15:45:30 -0500

    objeto post en forma de clase

--- weekly_mission_2/exercices/Ejercicio3/clasesFacebook.js
+++ weekly_mission_2/exercices/Ejercicio3/clasesFacebook.js
@@ -12,5 +12,18 @@ class User {
     }
 }
 
+class Post {
+    constructor(privacy, post, location, tagFriends){
+        this.privacy = privacy
+        this.post = post
+        this.location = location
+        this.tagFriends = tagFriends
+    }
+    getLocation(){
+        return `Este post fue hecho en: ${this.location}`
+    }
+}
+
 const user = new User("Daniel Espinoza", 568, "Morelia", "19 Julio", 4, "profile.jpg")
-console.log(user)
+const post = new Post("Public", "Hola estoy modelando con clases!", "Morelia", "Carlo")
+console.log(post)
