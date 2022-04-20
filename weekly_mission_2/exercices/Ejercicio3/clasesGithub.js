4b03532290036de2b49137f1b8dd6951f80b659a
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-20 15:30:49 -0500

    objeto pullrequest en forma de clase

--- weekly_mission_2/exercices/Ejercicio3/clasesGithub.js
+++ weekly_mission_2/exercices/Ejercicio3/clasesGithub.js
@@ -36,6 +36,23 @@ class Issue {
     }
 }
 
+class PullRequest {
+    constructor(title, branchName, dateCreated, status, repositoryNameAssociated){
+        this.title = title
+        this.branchName = branchName
+        this.dateCreated = dateCreated
+        this.status = status
+        this.repositoryNameAssociated = repositoryNameAssociated
+    }
+    getStatus(){
+        return this.status
+    }
+    getTitleAndBranch(){
+        return this.title + this.branchName
+    }
+}
+
 const launchXBackend = new Repo("LaunchX", "Carlogilmar", "JavaScript", 100, 199, 299, 10, 10)
 const issue = new Issue("Semana 2", "LaunchXBackend", "active", 23, "Live2", "MC", "17/04/2022", "2 days ago")
-console.log(issue)
+const blog = new PullRequest("Haciendo Correcciones", "Default", "16/04/2022", "open", "BlogLaunchX")
+console.log(blog)
